ad22a7e004f9140d4caa2dad96eba9dc60f52fd0
newhask777 <newhaskellisp@gmail.com>
2023-10-15 19:09:12 +0300

    crud

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -178,7 +178,7 @@
           </li>
 
           <li class="nav-item">
-            <a href="pages/gallery.html" class="nav-link">
+            <a href="{{ route('category.index') }}" class="nav-link">
               <i class="nav-icon fas fa-list"></i>
               <p>
                 Категории
