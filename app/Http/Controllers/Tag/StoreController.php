78efa58b3ac36c0b8bb93c698acf3bd6f6339eab
newhask777 <newhaskellisp@gmail.com>
2023-10-21 18:12:23 +0300

    users

--- app/Http/Controllers/Tag/StoreController.php
+++ app/Http/Controllers/Tag/StoreController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Tag;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Category\StoreRequest;
+use App\Http\Requests\Tag\StoreRequest;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.main')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Пользователи</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Главная</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <a href="{{ route('user.create') }}" class="btn btn-primary">Добавить Пользователя</a>
+                        </div>
+
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Наименование</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    @foreach ($users as $user)
+                                        <tr>
+                                            <td>{{ $user->id }}</td>
+                                            <td><a href="{{ route('tag.show', $user->id) }}">{{ $user->title }}</a></td>
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+    </div>
+@endsection
