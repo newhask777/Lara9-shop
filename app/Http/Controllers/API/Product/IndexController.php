7de1e3d6b8a539045d895d59ae90989d24dce939
newhask777 <newhaskellisp@gmail.com>
2023-11-04 11:26:52 +0300

    f

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,7 @@ class IndexController extends Controller
     public function __invoke()
     {
         $products = Product::all();
-        return $products;
+        return ProductResource::collection($products);
 
     }
 }
